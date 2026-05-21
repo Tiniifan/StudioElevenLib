acb3eb371e36844703442d7cb8f3e777b3224c54
Tiniifan <30804632+Tiniifan@users.noreply.github.com>
2026-05-21 19:52:24 +0200

    Add RLE Compression

--- StudioElevenLib/Level5/Compression/RLE/RLE.cs
+++ StudioElevenLib/Level5/Compression/RLE/RLE.cs
@@ -1,16 +1,100 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace StudioElevenLib.Level5.Compression.RLE
 {
     public class RLE : ICompression
     {
+        /// <summary>
+        /// Compresses data using RLE compression.
+        /// </summary>
         public byte[] Compress(byte[] indata)
         {
-            // Not Implemented
+            // Check maximum supported file size
+            if (indata.Length > 0x1FFFFFFF)
+                throw new Exception("File is too big to be compressed with Level5 compressions!");
 
-            return null;
+            using (MemoryStream output = new MemoryStream())
+            {
+                // Write compression header
+                var header = new[]
+                {
+            (byte)((byte)(indata.Length << 3) | 4),
+            (byte)(indata.Length >> 5),
+            (byte)(indata.Length >> 13),
+            (byte)(indata.Length >> 21)
+        };
+
+                output.Write(header, 0, 4);
+
+                int i = 0;
+
+                while (i < indata.Length)
+                {
+                    // Calculate current run length
+                    int runLen = 1;
+
+                    while (i + runLen < indata.Length &&
+                           indata[i + runLen] == indata[i] &&
+                           runLen < 0x82)
+                    {
+                        runLen++;
+                    }
+
+                    if (runLen >= 3)
+                    {
+                        // Write compressed block
+                        output.WriteByte((byte)(0x80 | (runLen - 3)));
+                        output.WriteByte(indata[i]);
+
+                        i += runLen;
+                    }
+                    else
+                    {
+                        // Write raw block
+                        int rawStart = i;
+                        int rawLen = 0;
+
+                        while (i < indata.Length && rawLen < 0x80)
+                        {
+                            // Stop if a compressible run is detected
+                            int peek = 1;
+
+                            while (i + peek < indata.Length &&
+                                   indata[i + peek] == indata[i] &&
+                                   peek < 3)
+                            {
+                                peek++;
+                            }
+
+                            if (peek >= 3)
+                                break;
+
+                            rawLen++;
+                            i++;
+                        }
+
+                        // Safety fallback
+                        if (rawLen == 0)
+                        {
+                            rawLen = 1;
+                            i++;
+                        }
+
+                        output.WriteByte((byte)(rawLen - 1));
+                        output.Write(indata, rawStart, rawLen);
+                    }
+                }
+
+                // Return compressed data
+                return output.ToArray();
+            }
         }
 
+        /// <summary>
+        /// Decompresses data using RLE compression.
+        /// </summary>
         public byte[] Decompress(byte[] instream)
         {
             long inLength = instream.Length;
